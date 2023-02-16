3a0252c26c1c1f82ec4f964744b63df0dd054d03
브랜치가머지 <rlagptjd2002@naver.com>
2023-02-16 13:15:18 +0900

    d

---
+++ pgms/src/pgms/prac014.java
@@ -0,0 +1,23 @@
+package pgms;
+
+public class prac014 {
+
+	public static void main(String[] args) {
+		int[] a = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+		int n = 3;
+		String b ="";
+		
+		for(int i=0; i<a.length;i++) {
+			if(a[i]%3==0) {
+				b += Integer.toString(a[i]);
+			}
+		}
+		int[] answer = new int[b.length()];
+		for(int i=0; i<b.length(); i++) {
+			answer[i] = b.charAt(i)-'0';
+			System.out.print(answer[i]);
+		}
+		
+	}
+
+}
