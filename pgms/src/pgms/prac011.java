6d3e55be0e648c7bf5c8abaa513d1f2a55c6ea17
브랜치가머지 <rlagptjd2002@naver.com>
2023-02-16 09:08:20 +0900

    aa

--- pgms/src/pgms/prac011.java
+++ pgms/src/pgms/prac011.java
@@ -3,7 +3,16 @@ package pgms;
 public class prac011 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String rsp = "205";
+		
+		rsp = rsp.replace("2", "묵");
+		rsp = rsp.replace("0", "빠");
+		rsp = rsp.replace("5", "찌");
+		rsp = rsp.replace("묵", "0");
+		rsp = rsp.replace("빠", "5");
+		rsp = rsp.replace("찌", "2");
+		
+		System.out.println(rsp);
 
 	}
 
