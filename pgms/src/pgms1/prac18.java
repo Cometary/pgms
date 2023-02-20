92558bd2eda8a60042483b8bb26d8ee7b3594c5c
브랜치가머지 <rlagptjd2002@naver.com>
2023-02-20 13:14:23 +0900

    a

--- pgms/src/pgms1/prac18.java
+++ pgms/src/pgms1/prac18.java
@@ -4,9 +4,32 @@ public class prac18 {
 
 	public static void main(String[] args) {
 		
+		int[] array = {6, 1, -2, -4, 21, 19, 28};
+		int n = -3;
+		int answer = array[0];
+		int tmp = 0;
 		
+		int differ = array[0]-n;
+		if(differ<0) {
+			differ = -differ;
+		}
 		
-		
+		for(int i=1; i<array.length; i++) {
+			tmp = array[i]-n;
+			if(tmp<0) {
+				tmp = -tmp;
+			}
+			if(differ==tmp && answer>array[i]) {
+				answer = array[i];
+			}else if(differ==tmp && answer<array[i]) {
+				
+			}else if(differ>tmp) {
+				differ = tmp;
+				answer = array[i];
+			}
+			
+		}
+		System.out.println(answer);
 		
 		
 
