58902e9070691a56e0de32fb031c18231a1873e2
브랜치가머지 <rlagptjd2002@naver.com>
2023-02-22 10:42:39 +0900

    a

---
+++ pgms/src/pgms2/prac03.java
@@ -0,0 +1,16 @@
+package pgms2;
+
+public class prac03 {
+
+	public static void main(String[] args) {
+		String my = "aAb1B2cC34oOp";
+		my = my.replaceAll("[^0-9]", "");
+		System.out.println(my);
+		
+		
+		
+		
+
+	}
+
+}
