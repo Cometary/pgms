40e503518aed90211ba2179823ebd0a844e726cf
브랜치가머지 <rlagptjd2002@naver.com>
2023-02-23 10:51:51 +0900

    a

--- pgms/src/pgms2/prac03.java
+++ pgms/src/pgms2/prac03.java
@@ -4,15 +4,18 @@ public class prac03 {
 
 	public static void main(String[] args) {
 		String my = "aAb1B2cC34oOp";
+		int answer=0;
 //		my = my.replaceAll("[^0-9]", "");
 		System.out.println(my);
 		String[] az = my.split("[a-zA-Z]");
 		for(int i=0; i<az.length; i++) {
 //			System.out.println(" "+az[i]+" ");
-			
+			if(!az[i].isEmpty()) {
+				answer += Integer.parseInt(az[i]);
+			}
 			
 		}
-		
+		System.out.println(answer);
 		
 		
 
