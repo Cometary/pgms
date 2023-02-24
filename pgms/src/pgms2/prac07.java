47e248038be9288ca574157d0b0e7ad2516e3306
브랜치가머지 <rlagptjd2002@naver.com>
2023-02-24 13:17:00 +0900

    a

---
+++ pgms/src/pgms2/prac07.java
@@ -0,0 +1,16 @@
+package pgms2;
+
+public class prac07 {
+
+	public static void main(String[] args) {
+		// [1, 2, 3, 4, 5, 6]	5
+		int[] numbers = {1, 2, 3};
+		int k = 3;
+		int answer;
+		answer = numbers[(2*k-1)%numbers.length-1];
+		System.out.println(answer);
+		
+		
+	}
+
+}
