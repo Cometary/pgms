d18aea2dcb8e466c8ae990f3ea0c5c4c9df34e85
브랜치가머지 <rlagptjd2002@naver.com>
2023-02-27 09:31:30 +0900

    s

--- pgms/src/pgms2/prac07.java
+++ pgms/src/pgms2/prac07.java
@@ -7,7 +7,11 @@ public class prac07 {
 		int[] numbers = {1, 2, 3, 4, 5};
 		int k = 6;
 		int answer;
+		if((2*k-1)%numbers.length-1<0){
+            answer = numbers[numbers.length-1];
+        }else{
 		answer = numbers[(2*k-1)%numbers.length-1];
+        }
 		System.out.println(answer);
 		
 		
