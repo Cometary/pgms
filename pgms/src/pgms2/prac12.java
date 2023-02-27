2a614878b4a0eea6208cd4d3fea3e712896119c7
브랜치가머지 <rlagptjd2002@naver.com>
2023-02-27 15:19:41 +0900

    z

---
+++ pgms/src/pgms2/prac12.java
@@ -0,0 +1,32 @@
+package pgms2;
+
+import java.math.BigInteger;
+
+public class prac12 {
+
+	public static void main(String[] args) {
+		int balls = 30;
+		int share = 15;
+		int answer = 0;
+//		int n=1;
+		int m=1;
+		int nm =1;
+		BigInteger n = new BigInteger("1");
+
+//		for(int i=balls; i>0; i--) {
+//			n.;
+//			if(i<=balls-share) {
+//				n/=i;
+//			}
+//			if(i<=share) {
+//				n/=i;
+//			}
+//		}
+		
+//		answer = n;
+		System.out.println(answer);
+		
+
+	}
+
+}
