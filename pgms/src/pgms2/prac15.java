2f58c615d1fda79557a64c2a918af2abd6a8805f
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-03 11:30:12 +0900

    d

---
+++ pgms/src/pgms2/prac15.java
@@ -0,0 +1,17 @@
+package pgms2;
+
+public class prac15 {
+
+	public static void main(String[] args) {
+		int M = 2;
+		int N = 5;
+		int result = 0;
+		
+		result += M-1;
+		result += (N-1)*M;
+		
+		System.out.println(result);
+
+	}
+
+}
