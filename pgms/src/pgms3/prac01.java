c7c7c134ee20926558f1865a1db3aa8be35bb191
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-05 15:25:53 +0900

    a

---
+++ pgms/src/pgms3/prac01.java
@@ -0,0 +1,17 @@
+package pgms3;
+
+public class prac01 {
+
+	public static void main(String[] args) {
+		String A = "hello";
+		String B = "lohe";
+		String C = B+B;
+		
+		
+		System.out.println(C.indexOf(A));
+		
+		
+		
+	}
+
+}
