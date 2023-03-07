43a4bc6f85f6cb1c12b828a69b8db744ab885d14
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-07 09:07:30 +0900

    s

--- pgms/src/pgms3/prac04.java
+++ pgms/src/pgms3/prac04.java
@@ -3,8 +3,42 @@ package pgms3;
 public class prac04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] quiz = {"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"};
+		String[][] math = new String[quiz.length][5];
+		String[] answer = new String[quiz.length];
+		
+		for(int i=0; i<quiz.length; i++) {
+			math[i] = quiz[i].split(" ");
+			switch(math[i][1]) {
+			case "+" :
+				if(Integer.parseInt(math[i][0]) + Integer.parseInt(math[i][2]) == Integer.parseInt(math[i][4])) {
+					answer[i] = "O";
+				}else {
+					answer[i] = "X";
+				}
+				break;
+			case "-" :
+				if(Integer.parseInt(math[i][0]) - Integer.parseInt(math[i][2]) == Integer.parseInt(math[i][4])) {
+					answer[i] = "O";
+				}else {
+					answer[i] = "X";
+				}
+				break;
+				
+			}
+			System.out.println("answer["+i+"] : "+answer[i]);
+		}
+		
+//		for(int i=0; i<quiz.length; i++) {
+//			for(int j=0; j<5; j++) {
+//				System.out.println("math["+i+"]["+j+"] : "+math[i][j]);
+//			}
+//		}
+		
+		
+		
+		
+		
 	}
 
 }
