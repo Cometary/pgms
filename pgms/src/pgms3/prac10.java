a0aa9c5442590a0199ab2f36bcf681eb9a4052d4
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-09 09:08:53 +0900

    w

--- pgms/src/pgms3/prac10.java
+++ pgms/src/pgms3/prac10.java
@@ -3,8 +3,22 @@ package pgms3;
 public class prac10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] babbling = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+		int[] bab = {0, 0, 0, 0};
+		int answer=0;
+		
+		for(int i=0;i<babbling.length; i++) {
+				babbling[i] = babbling[i].replaceAll("aya", " ");
+				babbling[i] = babbling[i].replaceAll("ye", " ");
+				babbling[i] = babbling[i].replaceAll("woo", " ");
+				babbling[i] = babbling[i].replaceAll("ma", " ");
+				babbling[i] = babbling[i].replaceAll(" ", "");				
+				System.out.println(babbling[i]);
+				if(babbling[i].equals("")) {
+					answer++;
+				}
+		}
+		System.out.println(answer);
 	}
 
 }
