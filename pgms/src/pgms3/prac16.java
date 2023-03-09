d63b5f2841954aaa26efcf6c74f0bb18d49f2cff
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-10 08:59:37 +0900

    a

---
+++ pgms/src/pgms3/prac16.java
@@ -0,0 +1,17 @@
+package pgms3;
+
+public class prac16 {
+
+	public static void main(String[] args) {
+		int[][] dots = {
+				{1,4}, {9,2}, {3,8}, {11, 6}
+		};
+		int[] ans = new int[6];
+		
+		
+		
+		
+
+	}
+
+}
