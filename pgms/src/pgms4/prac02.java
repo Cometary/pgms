b5b6ca278bb90eb4ad116c4b6762b11955e3269e
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-11 14:52:13 +0900

    e

--- pgms/src/pgms4/prac02.java
+++ pgms/src/pgms4/prac02.java
@@ -3,8 +3,17 @@ package pgms4;
 public class prac02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x = 2;
+		int n = 5;
+		int a = x;
+		long[] answer = new long[n];
+		
+		for(int i=0; i<n; i++) {
+			answer[i] = x;
+			x+=a;
+		}
 
+		
 	}
 
 }
