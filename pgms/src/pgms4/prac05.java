293507751c6344cc60adba3fe42023d42728367b
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-12 15:41:16 +0900

    r

--- pgms/src/pgms4/prac05.java
+++ pgms/src/pgms4/prac05.java
@@ -11,14 +11,18 @@ public class prac05 {
 		String m = Long.toString(n);
 		char[] car = m.toCharArray();
 		ArrayList<Integer> arr = new ArrayList<>();
+		
 		for(int i=0; i<car.length; i++) {
 			arr.add((int)car[i]-48);
 		}
+		
 		Collections.sort(arr);
 		m="";
+		
 		for(int i=arr.size()-1; i>=0; i--) {
 			m+= arr.get(i);
 		}
+		
 		answer = Long.parseLong(m);
 		System.out.println(answer);
 		
