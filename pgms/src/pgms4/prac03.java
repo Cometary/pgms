bf82c798e5d1cb3e97150c0b06bc90da0811a043
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-12 13:38:20 +0900

    w

--- pgms/src/pgms4/prac03.java
+++ pgms/src/pgms4/prac03.java
@@ -3,8 +3,25 @@ package pgms4;
 public class prac03 {
 
 	public static void main(String[] args) {
+		String s = "pPoooyY";
+		s = s.toLowerCase();
+		int p=0;
+		int y=0;
+		boolean answer = true;
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i)=='p') {
+				p++;
+			}
+			if(s.charAt(i)=='y') {
+				y++;
+			}
+		}
+		if(p!=y) {
+			answer = false;
+		}
 		
-
+		
+		
 	}
 
 }
