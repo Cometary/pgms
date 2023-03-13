d596ea8503b6e08d119a83bf503652086e7d2b36
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-13 20:11:22 +0900

    f

--- pgms/src/pgms4/prac16.java
+++ pgms/src/pgms4/prac16.java
@@ -1,15 +1,35 @@
 package pgms4;
 
-import java.util.Arrays;
-
 public class prac16 {
 
 	public static void main(String[] args) {
 		int[] arr = {4,3,2,1};
-		int[] brr = arr;
+		int min =arr[0];
+		int mini = 0;
+		int[] answer;
 		
-		Arrays.sort(arr);
+		if(arr.length==1) {
+			answer = new int[1];
+			answer[0] = -1;
+		}else {
+		answer = new int[arr.length-1];
+		for(int i=1; i<arr.length; i++) {
+			if(min>arr[i]) {
+				min = arr[i];
+				mini = i;
+			}
+		}
 		
+		for(int i=0; i<arr.length; i++) {
+			if(i<mini) {
+				answer[i] = arr[i];
+			}else if(i==mini) {
+				
+			}else {
+				answer[i-1] = arr[i];
+			}
+		}
+		}
 		
 		
 		
