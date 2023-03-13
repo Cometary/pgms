98d07c336cf0cb70d8820868559b6a3f4762b75c
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-13 15:40:41 +0900

    3

---
+++ pgms/src/pgms4/prac16.java
@@ -0,0 +1,20 @@
+package pgms4;
+
+import java.util.Arrays;
+
+public class prac16 {
+
+	public static void main(String[] args) {
+		int[] arr = {4,3,2,1};
+		int[] brr = arr;
+		
+		Arrays.sort(arr);
+		
+		
+		
+		
+		
+
+	}
+
+}
