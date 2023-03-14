8ca53a513107c722daba61bded92e0730d8027be
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-14 10:33:24 +0900

    y

---
+++ pgms/src/pgms5/prac06.java
@@ -0,0 +1,28 @@
+package pgms5;
+
+public class prac06 {
+
+	public static void main(String[] args) {
+		int left = 24;
+		int right = 27;
+		int count = 0;
+		int answer =0;
+		for(int i=left; i<=right; i++) {
+			count=0;
+			for(int j=1; j<=i; j++) {
+				if(i%j==0) {
+					count++;
+				}
+			}
+			if(count%2==0) {
+				answer += i;
+			}else {
+				answer -= i;
+			}
+		}
+		System.out.println(answer);
+		
+		
+	}
+
+}
