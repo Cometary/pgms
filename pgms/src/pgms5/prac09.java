658e07d4203fe6261a3e338f704bef23aa480a56
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-15 08:59:37 +0900

    q

--- pgms/src/pgms5/prac09.java
+++ pgms/src/pgms5/prac09.java
@@ -5,7 +5,22 @@ public class prac09 {
 	public static void main(String[] args) {
 		int[][] arr1 = {{1,2}, {2,3}};
 		int[][] arr2 = {{3,4}, {5,6}};
-
+		
+		for(int i=0; i<arr1.length; i++) {
+			for(int j=0; j<arr1[i].length; j++) {
+				arr1[i][j] += arr2[i][j];
+			}
+		}
+		
+		
+//		System.out.println("arr1["+i+"]["+j+"] : "+arr1[i][j]);
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
