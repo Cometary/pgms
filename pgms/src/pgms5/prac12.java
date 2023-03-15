7274ca8569a0bb664553bc88c2c9ba6a4bf34574
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-15 10:20:18 +0900

    v

---
+++ pgms/src/pgms5/prac12.java
@@ -0,0 +1,25 @@
+package pgms5;
+
+public class prac12 {
+
+	public static void main(String[] args) {
+		int[] arr = {1,1,3,3,0,1,1};
+		java.util.ArrayList<Integer> arl = new java.util.ArrayList<>();
+		arl.add(arr[0]);
+		
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i-1]!=arr[i]) {
+				arl.add(arr[i]);
+			}
+		}
+		int[] answer = new int[arl.size()];
+		for(int i=0; i<arl.size(); i++) {
+			answer[i] = arl.get(i);
+		}
+		
+		
+		
+
+	}
+
+}
