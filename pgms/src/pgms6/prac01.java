c17bd6e18d19fa08f5ddbd9685b1ade60462d0f5
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-18 13:51:29 +0900

    d

--- pgms/src/pgms6/prac01.java
+++ pgms/src/pgms6/prac01.java
@@ -3,8 +3,23 @@ package pgms6;
 public class prac01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] number = {-2, 3, 0, 2, -5};
+		int result = 0;
+		
+		for(int i=1; i<number.length-1; i++) {
+			for(int j=i+1; j<number.length; j++) {
+				if(number[i]+number[j]+number[i-1]==0) {
+					result++;
+					System.out.println(number[i]+" "+number[j]+" "+number[i-1]);
+				}
+			}
+		}
+		
+		System.out.println(result);
+		
+		
+		
+		
 	}
 
 }
