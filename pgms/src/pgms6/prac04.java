366d49cba8cacbc8f3af50305997f34f543b1f19
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-21 09:34:14 +0900

    r

--- pgms/src/pgms6/prac04.java
+++ pgms/src/pgms6/prac04.java
@@ -3,8 +3,24 @@ package pgms6;
 public class prac04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s = "2three45sixseven";
+		int answer = 0;
+		
+		s= s.replaceAll("zero", "0");
+		s= s.replaceAll("one", "1");
+		s= s.replaceAll("two", "2");
+		s= s.replaceAll("three", "3");
+		s= s.replaceAll("four", "4");
+		s= s.replaceAll("five", "5");
+		s= s.replaceAll("six", "6");
+		s= s.replaceAll("seven", "7");
+		s= s.replaceAll("eight", "8");
+		s= s.replaceAll("nine", "9");
+		
+		answer = Integer.parseInt(s);
+		System.out.println(answer);
+		
+		
 	}
 
 }
