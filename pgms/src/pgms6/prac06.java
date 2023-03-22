5fec2a2eeeb9498f61d6d27c6211eec385dd0ce7
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-22 09:02:29 +0900

    w

---
+++ pgms/src/pgms6/prac06.java
@@ -0,0 +1,21 @@
+package pgms6;
+
+public class prac06 {
+
+	public static void main(String[] args) {
+		String t ="3141592";
+		String p = "271";
+		int answer = 0;
+		int leng = p.length();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ pgms/src/pgms6/prac07.java
@@ -0,0 +1,17 @@
+package pgms6;
+
+public class prac07 {
+
+	public static void main(String[] args) {
+		String s = "aukks";
+		String skip = "wbqd";
+		int index = 5;
+		
+		
+		
+		
+		
+
+	}
+
+}
