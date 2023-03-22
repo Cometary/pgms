27b26891af59e743b3e5cb56c423db3023ed4c55
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-22 10:05:35 +0900

    q

--- pgms/src/pgms6/prac07.java
+++ pgms/src/pgms6/prac07.java
@@ -3,15 +3,32 @@ package pgms6;
 public class prac07 {
 
 	public static void main(String[] args) {
-		String s = "aukks";
-		String skip = "wbqd";
-		int index = 5;
-		
-		
-		
-		
-		
-
+		//문제
+		String s = "aukksaazzazazazazazaazzzzz";
+		String skip = "bcdefghijk";
+		int index = 20;
+		//사전준비
+		String answer = "";
+		String[] alpha = new String[26-skip.length()];
+		String alphabet ="";
+		int num = 0;
+		for(int i=97; i<123; i++) {
+			if(!skip.contains(Character.toString((char)i))) {
+				alpha[num] = Character.toString((char)i);
+				alphabet += Character.toString((char)i);
+				num++;
+			}
+		}
+		//추출
+		int tmp;
+		for(int i=0; i<s.length(); i++) {
+			tmp = alphabet.indexOf(s.charAt(i))+index;
+			while(tmp>=alphabet.length()) {
+				tmp-=alphabet.length();
+			}
+			answer += alpha[tmp];
+		}
+		System.out.println(answer);
 	}
 
 }
