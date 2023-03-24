686103b6fa23c97243a19f59fa68bc64bb2bdb62
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-24 10:25:16 +0900

    t

--- pgms/src/pgms6/prac09.java
+++ pgms/src/pgms6/prac09.java
@@ -3,8 +3,25 @@ package pgms6;
 public class prac09 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] food = {1, 3, 4, 6};
+		String before = "";
+		String after = "";
+		
+		for(int i=1; i<food.length; i++) {
+			for(int j=0; j<food[i]/2; j++) {
+				before += ""+i;
+			}
+		}
+		StringBuffer sb = new StringBuffer(before);
+		after = sb.reverse().toString();
+		
+		String answer = "";
+		answer = before+"0"+after;
+		System.out.println(answer);
+		
+		
+		
+		
 	}
 
 }
