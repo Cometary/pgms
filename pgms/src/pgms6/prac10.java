06451878367fc4693987fb0e5aecb0a74bbe18e0
브랜치가머지 <rlagptjd2002@naver.com>
2023-03-25 13:54:11 +0900

    r

--- pgms/src/pgms6/prac10.java
+++ pgms/src/pgms6/prac10.java
@@ -3,8 +3,25 @@ package pgms6;
 public class prac10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a = 5;
+		int b = 24;
+		
+		String[] week = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+		int day =0;
+		
+		for(int i=1; i<a; i++) {
+			if(i==2) {
+				day+=29;
+			}else if(i==1 || i==3 || i==5 || i==7 || i==8 || i==10 || i==12) {
+				day+=31;
+			}else {
+				day+=30;
+			}
+		}
+		day+=b;
+		
+		String answer = week[(day-1)%7];
+		
 	}
 
 }
