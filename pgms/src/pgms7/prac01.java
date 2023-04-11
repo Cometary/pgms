e241167f43ff6b09cbb0627805d78ab697f670db
Cometary <109892844+Cometary@users.noreply.github.com>
2023-04-11 09:09:54 +0900

    q

--- pgms/src/pgms7/prac01.java
+++ pgms/src/pgms7/prac01.java
@@ -3,11 +3,40 @@ package pgms7;
 public class prac01 {
 
 	public static void main(String[] args) {
-		String[] cards1 = {"i", "drink", "water"};
-		String[] cards2 = {"want", "to"};
-		String[] goal = {"i", "want", "to", "drink", "water"};
-		System.out.println();
+		String[] cards1 = {"a", "b", "c"};
+		String[] cards2 = {"d", "e"};
+		String[] goal = {"b", "c", "d", "e"};
+		int tmp1=-1;
+		int tmp2=-1;
+		String answer = "Yes";
 		
+		for(int i=0; i<cards1.length; i++) {
+			for(int j=0; j<goal.length; j++) {
+				if(cards1[i].equals(goal[j])) {
+					if(tmp1>j) {
+						answer = "No";
+					}
+					tmp1 = j;
+				}
+			}
+			if(tmp1==-1) {
+				answer = "No";				
+			}
+		}
+		for(int i=0; i<cards2.length; i++) {
+			for(int j=0; j<goal.length; j++) {
+				if(cards2[i].equals(goal[j])) {
+					if(tmp2>j) {
+						answer = "No";
+					}
+					tmp2 = j;
+				}
+			}
+			if(tmp2==-1) {
+				answer = "No";				
+			}
+		}
+		System.out.println(answer);
 		
 		
 
