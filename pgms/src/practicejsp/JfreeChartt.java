8aa085da3c8486f1a071125c273702eaed0b6ac2
Cometary <109892844+Cometary@users.noreply.github.com>
2023-04-28 08:39:59 +0900

    qw

---
+++ pgms/src/practicejsp/JfreeChartt.java
@@ -0,0 +1,14 @@
+package practicejsp;
+
+public class JfreeChartt {
+//	// 1. 데이터를 생성한다.
+//			DefaultPieDataset dataset = new DefaultPieDataset();
+//			dataset.setValue("Son-OGong", 63.2);
+//			dataset.setValue("Jer-PalGae", 37.9);
+//			dataset.setValue("Sa-OJung", 29.5);
+//			// 2. 차트를 만든다. 
+//			JFreeChart chart = ChartFactory.createPieChart("Energy Power", dataset, true, true, false );
+//			// 4. 차트를 보여준다.
+//			ServletOutputStream out = response.getOutputStream();
+//			ChartUtils.writeChartAsPNG(out, chart, 400, 400);
+}
