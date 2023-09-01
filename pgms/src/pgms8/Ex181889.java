c182635ddb08c877733be119cdeb5f97fba84beb
Cometary <109892844+Cometary@users.noreply.github.com>
2023-09-01 16:33:29 +0900

    Level 0 181889

---
+++ pgms/src/pgms8/Ex181889.java
@@ -0,0 +1,15 @@
+package pgms8;
+
+public class Ex181889 {
+
+	public static void main(String[] args) {
+		int[] num_list = {5, 2, 1, 7, 5};
+		int n = 3;
+		
+		int[] answer = new int[n]; 
+		for(int i=0; i<n; i++) {
+			answer[i] = num_list[i];
+		}
+	}
+
+}
