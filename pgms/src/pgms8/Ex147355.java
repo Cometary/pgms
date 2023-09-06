6e768505891678bbf3c71df5768b05dce2e26869
Cometary <109892844+Cometary@users.noreply.github.com>
2023-09-06 17:03:52 +0900

    Level 1 147355

---
+++ pgms/src/pgms8/Ex147355.java
@@ -0,0 +1,21 @@
+package pgms8;
+
+public class Ex147355 {
+
+	public static void main(String[] args) {
+		String t = "10203";	
+		String p = "10203";
+		int answer = 0;
+		int tl = t.length(); //7
+		int pl = p.length(); //3
+		
+		for(int i = 0; i<=tl-pl; i++) {
+			answer += Long.parseLong(p) >= Long.parseLong(t.substring(i, i+pl)) ? 1 : 0;
+		}
+	
+		
+		System.out.println(answer);
+
+	}
+
+}
