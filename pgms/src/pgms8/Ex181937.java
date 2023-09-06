e20eb0b85af40e0c350b5476c06d9ee53e2ad2b9
Cometary <109892844+Cometary@users.noreply.github.com>
2023-09-06 13:21:06 +0900

    Level 0 181937

---
+++ pgms/src/pgms8/Ex181937.java
@@ -0,0 +1,16 @@
+package pgms8;
+
+public class Ex181937 {
+
+	public static void main(String[] args) {
+		int num = 98;	
+		int n = 3;
+		int answer;
+		
+		answer = num%n==0 ? 1 : 0;
+		
+		System.out.println(answer);
+		
+	}
+
+}
